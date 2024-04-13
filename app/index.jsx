5890d1c318760cfc4d06aab1cd97fca4ada54683
Prajwal A K <prajwalhassan7@gmail.com>
2024-04-13 23:50:15 +0530

    Files and folder structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import {  Text, View } from "react-native";
 
 export default function App(){
     return(
         <View className="flex-1 bg-black text-white items-center justify-center" >
             <Text className="font-semibold text-primary">Open uo App.js to start working</Text>
+            <Link href="/home" className="text-blue-700">Go To Home</Link>
         </View>
     )
 }
