6d160f1cc8daab9027a6898041367aa766d5bfb2
Prajwal A K <prajwalhassan7@gmail.com>
2024-04-13 23:37:17 +0530

    Native Wind Setup

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import {  Text, View } from "react-native";
+
+export default function App(){
+    return(
+        <View className="flex-1 bg-black text-white items-center justify-center" >
+            <Text className="font-semibold text-primary">Open uo App.js to start working</Text>
+        </View>
+    )
+}
